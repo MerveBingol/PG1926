00bf79aff81b4a287a39b49ba01906b9ca35bc00
MerveBingol <47832327+MerveBingol@users.noreply.github.com>
2021-01-30 16:13:13 +0300

    Python odev

---
+++ ilkuygulama.py
@@ -0,0 +1,13 @@
+
+a = 0
+while a < 101:
+    a += 1
+    if a%15==0:
+           print("FizzBuzz")
+    elif a%3==0:
+           print("fizz")
+    elif a%5==0:
+           print("Buzz")
+    else:
+           print(a)
+
